388ee64cfd6c9abaffdf2ffa593d164cb29760db
Alex Pereira <alex.pereira.6464@gmail.com>
2023-10-15 12:47:29 -0400

    Added temporary arm code. Changed naming conventions for setAllMotors[...] to setMotorArray[...]. Additionally, changed names of variables inside these functions to have 'my' at the beginning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GearGrinderWheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GearGrinderWheelTest.java
@@ -10,19 +10,22 @@ import java.util.List;
 @TeleOp (name="GG Wheel Test", group = "Testing")
 public class GearGrinderWheelTest extends LinearOpMode {
 
-    // Defining All the Motors!!
+    // Defining All the Motors for Wheels!!
     private DcMotor frontLeft, frontRight;
     private DcMotor backLeft, backRight;
+
+    // Defining All the Motors for the Arm!!
+    private DcMotor armBaseRotate;
 
     // Defining a Variable
     private double power;
     private double halfPower;
-    private boolean motorsRunning;
+    private boolean wheelsRunning;
 
     @Override
     public void runOpMode() throws InterruptedException {
         ArrayList<DcMotor> motors = new ArrayList<>();
-        // Motor Initialization!
+        // Wheel Motor Initialization!
         frontLeft = hardwareMap.get(DcMotor.class, "motor0");
         motors.add(frontLeft);
         frontRight = hardwareMap.get(DcMotor.class, "motor1");
@@ -31,10 +34,12 @@ public class GearGrinderWheelTest extends LinearOpMode {
         motors.add(backLeft);
         backRight = hardwareMap.get(DcMotor.class, "motor3");
         motors.add(backRight);
+        // Arm Motors Initialization!
+        armBaseRotate = hardwareMap.get(DcMotor.class, "motorE-0");
         // Variable Setting
         power = 5;
         // TODO - Make it so that there is a way that when a button is held, it goes at half power
-        motorsRunning = false;
+        wheelsRunning = false;
 
 
         // Waiting for Start Button to be Pushed
@@ -49,11 +54,11 @@ public class GearGrinderWheelTest extends LinearOpMode {
                 frontRight.setDirection(DcMotor.Direction.REVERSE);
                 backLeft.setDirection(DcMotor.Direction.FORWARD);
                 backRight.setDirection(DcMotor.Direction.REVERSE);
-                motorsRunning = true;
+                wheelsRunning = true;
             }
             // If Left-Stick is Neutral
-            if (gamepad1.left_stick_y == 0) {
-                motorsRunning = false;
+            if (gamepad1.left_stick_y == 0 && gamepad1.left_stick_x == 0) {
+                wheelsRunning = false;
             }
             // If Left-Stick is Pushed Backward
             if (gamepad1.left_stick_y < 0) {
@@ -62,38 +67,52 @@ public class GearGrinderWheelTest extends LinearOpMode {
                 frontLeft.setDirection(DcMotor.Direction.REVERSE);
                 backRight.setDirection(DcMotor.Direction.FORWARD);
                 backLeft.setDirection(DcMotor.Direction.REVERSE);
-                motorsRunning = true;
+                wheelsRunning = true;
             }
             // If Left-Stick is Pushed Left
             if (gamepad1.left_stick_x < 0) {
-                setAllMotorDirection(motors, true);
-                motorsRunning = true;
+                setMotorArrayDirection(motors, true);
+                wheelsRunning = true;
             }
             // If Left-Stick is Pushed Right
             if (gamepad1.left_stick_x > 0) {
-                setAllMotorDirection(motors, false);
-                motorsRunning = true;
+                setMotorArrayDirection(motors, false);
+                wheelsRunning = true;
             }
-            // Check if Motors Should Run
-            if (motorsRunning) {
-                setAllMotorPower(motors, power);
+            // If Right-Stick is Pushed Left
+            if (gamepad1.right_stick_x < 0) {
+                armBaseRotate.setDirection(DcMotor.Direction.FORWARD);
+                armBaseRotate.setPower(.2);
+            }
+            // If Right-Stick is Pushed Right
+            if (gamepad1.right_stick_x > 0) {
+                armBaseRotate.setDirection(DcMotor.Direction.REVERSE);
+                armBaseRotate.setPower(.2);
+            }
+            // If Right-Stick X is Neutral
+            if (gamepad1.right_stick_x == 0) {
+                armBaseRotate.setPower(0);
+            }
+            // Check if Wheels Should Run
+            if (wheelsRunning) {
+                setMotorArrayPower(motors, power);
             }
             else {
-                setAllMotorPower(motors, 0);
+                setMotorArrayPower(motors, 0);
             }
 
         }
     }
-    // Sets all power of all the motors to power
-    public void setAllMotorPower(ArrayList<DcMotor> motors, double power){
+    // Sets all power of all the motors in the Array to
+    public void setMotorArrayPower(ArrayList<DcMotor> myMotors, double myPower){
         // Through the power of arrays we make this teeny!
-        for (DcMotor motor : motors){
-            motor.setPower(power);
+        for (DcMotor motor : myMotors){
+            motor.setPower(myPower);
         }
     }
-    public void setAllMotorDirection(ArrayList<DcMotor> motors, boolean direction ) { //If Direction is true we turn right, else we turn left
-        for (DcMotor motor : motors){
-            if (direction) {
+    public void setMotorArrayDirection(ArrayList<DcMotor> myMotors, boolean myDirection ) { //If Direction is true we turn right, else we turn left
+        for (DcMotor motor : myMotors){
+            if (myDirection) {
                 motor.setDirection(DcMotor.Direction.FORWARD);
             }
             else {
